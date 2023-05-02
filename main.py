2f1e7e0b39bcffc22ee8529b5306b45a8ee299bf
Dev-ZC <75277561+Dev-ZC@users.noreply.github.com>
2023-05-01 23:24:31 -0700

    created database and sqlite connector

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import mysql.connector
+import sqlite3
 from datetime import datetime
+    
+conn = sqlite3.connect('Volunteers.db')
 
-db = mysql.connecter.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "root",
-    database = "volunteerdatabase"
-    )
+c = conn.cursor()
 
-mycursor = db.cursor()
 
-mycursor.execute("CREATE TABLE VolunteerInfo (name varchar(50) NOT NULL, created datetime NOT NULL, phone varchar(20), email varchar(50), gender ENUM ('M', 'F', 'O') NOT NULL, id int PRIMARY KEY NOT NULL AUTOINCREMENT)")
+#c.execute("""CREATE TABLE VolunteerInfo (
+#                    name varchar(50) NOT NULL, 
+#                    created datetime NOT NULL, 
+#                    phone varchar(20), 
+#                    email varchar(50), 
+#                    gender varchar(1) NOT NULL CHECK (gender IN ('M', 'F', 'O')), 
+#                    id INTEGER PRIMARY KEY AUTOINCREMENT)""")
+
+print(c.fetchall())
 
 #Make another table for availability
